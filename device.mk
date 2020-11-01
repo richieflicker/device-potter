840ff60f071f8ebe98a75fa1e984313e6fd8637a
Dybios <dibyajyoti.samal@gmail.com>
2020-11-01 18:26:51 +0530

    Build some additional camera packages

--- device.mk
+++ device.mk
@@ -118,8 +118,10 @@ PRODUCT_PACKAGES += \
     libbson \
     camera.device@1.0-impl \
     camera.device@3.2-impl:32 \
+    android.hardware.camera.provider@2.4 \
     android.hardware.camera.provider@2.4-impl:32 \
     android.hardware.camera.provider@2.4-service \
+    android.hardware.camera.common@1.0 \
     vendor.qti.hardware.camera.device@1.0 \
     vendor.qti.hardware.camera.device@1.0_vendor \
     libjustshoot_shim \
